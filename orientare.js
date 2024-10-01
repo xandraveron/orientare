c250c6e79af1d81859e190561426c78fbc189032
xandraveron <104696458+xandraveron@users.noreply.github.com>
2024-10-01 20:11:44 +0300

    Update orientare.js

--- orientare.js
+++ orientare.js
@@ -11,7 +11,7 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var centru = {x:canvas.width/2, y:canvas.height/2}
 var latura_patrat = 50;
-context.setTransform(1, 0, 0, 1, 0, 0);
+context.setTransform(1, 0, 0, 1, 0, 1);
 context.clearRect(0,0,canvas.width, canvas.height);
 context.translate(centru.x/2, centru.y/2);
 context.rotate(alpha * Math.PI / 180);
